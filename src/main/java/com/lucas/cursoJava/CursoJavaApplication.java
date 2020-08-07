e0110b394ad23f7f01dacef186f191b6cb18cb31
Lucas Amancio <lhamancio@gmail.com>
2020-08-06 23:51:26 -0300

    User entity and resource

--- src/main/java/com/lucas/cursoJava/CursoJavaApplication.java
+++ src/main/java/com/lucas/cursoJava/CursoJavaApplication.java
@@ -3,11 +3,17 @@ package com.lucas.cursoJava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.lucas.cursoJava.entities.User;
+
+
+
 @SpringBootApplication
 public class CursoJavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoJavaApplication.class, args);
+		
+		
 	}
 
 }
---
+++ src/main/java/com/lucas/cursoJava/entities/User.java
@@ -0,0 +1,43 @@
+package com.lucas.cursoJava.entities;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private long id;
+	private String name;
+	private String email;
+	private String phone;
+	private String password;
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+	
+	
+}
